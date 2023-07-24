a6770147ad076e56e5488ce4a157601281f67928
Mansi Sharma <78598152+Mansi-Sharma06@users.noreply.github.com>
2023-07-24 18:22:50 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (40.10%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,24 @@
+// class Solution {
+//     public double myPow(double x, int n) {
+        
+//         if(n==0){
+//             return 1;
+//         }
+        
+//         double smallAns = myPow(x, n-1);
+//         return smallAns * x;
+        
+//     }
+// }
+
+class Solution {
+  public double myPow(double x, long n) {
+    if (n == 0)
+      return 1;
+    if (n < 0)
+      return 1 / myPow(x, -n);
+    if (n % 2 == 1)
+      return x * myPow(x, n - 1);
+    return myPow(x * x, n / 2);
+  }
+}
