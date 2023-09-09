7c6dbc3ee1d47d5874dff7f5bdca702e2a5cef3c
Mansi Sharma <78598152+Mansi-Sharma06@users.noreply.github.com>
2023-09-09 18:22:06 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (61.50%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,15 +1,47 @@
 class Solution {
+    int[] dp;
     public int combinationSum4(int[] nums, int target) {
-        int [] dp = new int[target+1];
-        dp[0] =1;
-        for(int i =1; i<= target; i++){
-            for(int n : nums){
-                if(i>=n){
-                    dp[i] += dp[i-n];
+        
+        // bottom up approach
+        
+//         int [] dp = new int[target+1];
+//         dp[0] =1;
+//         for(int i =1; i<= target; i++){
+//             for(int n : nums){
+//                 if(i>=n){
+//                     dp[i] += dp[i-n];
+//                 }
+//             }
+//         }
+//         return dp[target];
+//     }
+// }
+        
+        // top down approach
+        
+          dp = new int[target+1];
+         Arrays.fill(dp, -1);
+         dp[0] =1;
+        
+        helper(nums,target);
+        return dp[target];
+    }
+    
+    int helper(int[] nums, int target){
+        
+        if(dp[target]> -1){
+            return dp[target];
+        }
+            
+            int res =0;
+            for(int i : nums){
+                if(i<=target){
+                    res += helper(nums, target-i);
                 }
             }
+            dp[target] = res;
+            return dp[target];
         }
-        return dp[target];
-        
     }
-}
+
+        
