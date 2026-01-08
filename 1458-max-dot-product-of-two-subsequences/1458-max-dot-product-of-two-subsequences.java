42fcaa5713e5e3e5c8050deeede88ac083b86d34
Mansi Sharma <78598152+Mansi-Sharma06@users.noreply.github.com>
2026-01-08 19:51:11 +0530

    Time: 9 ms (85.19%), Space: 46.6 MB (42.96%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,16 +1,44 @@
+import java.util.*;
+
 class Solution {
-  public int maxDotProduct(int[] nums1, int[] nums2) {
-    final int m = nums1.length;
-    final int n = nums2.length;
-    // dp[i][j] := max dot product of two subseqs nums[0..i) and nums2[0..j)
-    int[][] dp = new int[m + 1][n + 1];
-    Arrays.stream(dp).forEach(row -> Arrays.fill(row, Integer.MIN_VALUE));
+    int[] nums1, nums2;
+    int[][] memo;
+    int n, m;
+    final int NEG_INF = (int) -1e9;
 
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        dp[i + 1][j + 1] = Math.max(Math.max(dp[i][j + 1], dp[i + 1][j]),
-                                    Math.max(0, dp[i][j]) + nums1[i] * nums2[j]);
+    int dp(int i, int j) {
+        if (i == n || j == m)
+            return NEG_INF;
 
-    return dp[m][n];
-  }
+        if (memo[i][j] != Integer.MIN_VALUE)
+            return memo[i][j];
+
+        int take = nums1[i] * nums2[j];
+
+        int res = Math.max(
+            Math.max(
+                take + dp(i + 1, j + 1), // take both and continue
+                take                    // take and end here
+            ),
+            Math.max(
+                dp(i + 1, j),           // skip nums1[i]
+                dp(i, j + 1)            // skip nums2[j]
+            )
+        );
+
+        return memo[i][j] = res;
+    }
+
+    public int maxDotProduct(int[] a, int[] b) {
+        nums1 = a;
+        nums2 = b;
+        n = nums1.length;
+        m = nums2.length;
+
+        memo = new int[n][m];
+        for (int i = 0; i < n; i++)
+            Arrays.fill(memo[i], Integer.MIN_VALUE);
+
+        return dp(0, 0);
+    }
 }
