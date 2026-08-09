d5335704e9dadaeb864eb9ac85b5871cd77b2638
Mansi Sharma <78598152+Mansi-Sharma06@users.noreply.github.com>
2026-08-09 19:30:03 +0530

    Time: 3 ms (90.9%), Space: 44.1 MB (88.69%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,31 +1,42 @@
 class Solution {
-  public int stoneGameII(int[] piles) {
-    final int n = piles.length;
-    // dp[i][j] := max # of stones Alice can get w/ piles[i:] and M = j
-    dp = new int[n][n];
-    // suffixSum[i] := sum of piles[i:]
-    suffixSum = piles.clone();
 
-    for (int i = n - 2; i >= 0; --i)
-      suffixSum[i] += suffixSum[i + 1];
+    public int stoneGameII(int[] piles) {
+        // Store the suffix sum of all array elements.
+        int[] suffixSum = Arrays.copyOf(piles, piles.length);
 
-    return stoneGameII(0, 1);
-  }
+        for (int i = suffixSum.length - 2; i >= 0; i--) {
+            suffixSum[i] += suffixSum[i + 1];
+        }
+        return maxStones(suffixSum, 1, 0, new int[piles.length][piles.length]);
+    }
 
-  private int[][] dp;
-  private int[] suffixSum;
-
-  private int stoneGameII(int i, int M) {
-    if (i + 2 * M >= dp.length)
-      return suffixSum[i];
-    if (dp[i][M] > 0)
-      return dp[i][M];
-
-    int opponent = suffixSum[i];
-
-    for (int X = 1; X <= 2 * M; ++X)
-      opponent = Math.min(opponent, stoneGameII(i + X, Math.max(M, X)));
-
-    return dp[i][M] = suffixSum[i] - opponent;
-  }
+    private int maxStones(
+        int[] suffixSum,
+        int maxTillNow,
+        int currIndex,
+        int[][] memo
+    ) {
+        // If currIndex + 2*maxTillNow lies outside the array, pick all remaining stones.
+        if (currIndex + 2 * maxTillNow >= suffixSum.length) {
+            return suffixSum[currIndex];
+        }
+        if (memo[currIndex][maxTillNow] > 0) return memo[currIndex][maxTillNow];
+        int res = Integer.MAX_VALUE;
+        // Find the minimum value res for the next move possible.
+        for (int i = 1; i <= 2 * maxTillNow; i++) {
+            res = Math.min(
+                res,
+                maxStones(
+                    suffixSum,
+                    Math.max(i, maxTillNow),
+                    currIndex + i,
+                    memo
+                )
+            );
+        }
+        // Memoize the difference of suffixSum[p] and res. This denotes the maximum
+        // stones that can be picked.
+        memo[currIndex][maxTillNow] = suffixSum[currIndex] - res;
+        return memo[currIndex][maxTillNow];
+    }
 }
